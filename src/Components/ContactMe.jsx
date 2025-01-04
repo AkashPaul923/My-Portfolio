c4a67ff8d471b428a795a6dc89b5ad760c19dc33
AkashPaul923 <akashpaul923@gmail.com>
2025-01-05 02:05:02 +0600

    emailjs add

--- src/Components/ContactMe.jsx
+++ src/Components/ContactMe.jsx
@@ -1,29 +1,40 @@
-import React from 'react';
+import { useRef } from 'react';
+import emailjs from '@emailjs/browser';
 import { FaPhoneAlt, FaEnvelope, FaMapMarkerAlt, FaWhatsapp } from 'react-icons/fa';
 
 const ContactMe = () => {
+
+    const form = useRef();
+
+    const sendEmail = (e) => {
+        e.preventDefault();
+        console.log(form);
+        emailjs.sendForm('service_1mwkoln', 'template_6krjp6h', form.current, { publicKey: 'oYy2a2d_m8firz3yV', })
+        .then( 
+            () => {  console.log('SUCCESS!');}, 
+            (error) => { console.log('FAILED...', error.text);},
+        );
+    };
+
+
   return (
     <div id="contact-section" className="bg-secondaryBg py-20">
-        <h2 className="code-pro-font uppercase text-center font-bold text-primaryBtn text-3xl md:text-5xl">Contact Me</h2>
+        <h2 className="code-pro-font uppercase text-center font-bold text-primaryBtn text-3xl md:text-5xl mb-10">Contact Me</h2>
         <div className="max-w-screen-2xl mx-auto p-5 grid grid-cols-1 md:grid-cols-12 gap-5 lg:gap-10">
             {/* Email Section */}
             <div className="md:col-span-7 ">
-                <form className="card-body rounded-lg">
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text text-white">Email</span>
-                        </label>
-                        <input type="email" placeholder="Write Your email" className="input input-bordered bg-primaryBg" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text text-white">Email Body</span>
-                        </label>
-                        <textarea  placeholder="Write Email Body" className="min-h-32 input input-bordered bg-primaryBg" required />
-                    </div>
-                    <div className="form-control mt-6">
-                        <button className="btn btn-accent bg-primaryBtn">Send email</button>
-                    </div>
+                <form ref={form} onSubmit={sendEmail} className='max-w-xl grid grid-cols-12 gap-4'>
+                    
+                        <label className='text-lg col-span-3'>Name:</label>
+                        <input type="text" name="from_name" class="input input-bordered input-accent w-full  bg-transparent col-span-9" />
+    
+                        <label className='text-lg col-span-3'>Email:</label>
+                        <input type="email" name="from_email" class="input input-bordered input-accent w-full  bg-transparent col-span-9" />
+                    
+                        <label className='text-lg col-span-3'>Message:</label>
+                        <textarea name="message" class="input input-bordered input-accent w-full  bg-transparent col-span-9"/>
+                    
+                    <input type="submit" value="Send" className='btn btn-accent col-span-12 w-full mt-5' />
                 </form>
             </div>
 
--- src/Layout/Home.jsx
+++ src/Layout/Home.jsx
@@ -21,6 +21,7 @@ const Home = () => {
             smooth: "easeInOutQuart",
             });
         }
+        
     }, [location]);
 
 
