ed3007c6496e33b95c14303b4fd48eadfa5b0d46
AkashPaul923 <akashpaul923@gmail.com>
2025-02-06 23:54:50 +0600

    skills add

--- src/Components/Skills.jsx
+++ src/Components/Skills.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { useState } from "react";
+import Marquee from "react-fast-marquee";
 
 
 const Skills = () => {
@@ -13,13 +14,13 @@ const Skills = () => {
     return (
         <div id="skill-section" className="max-w-screen-2xl mx-auto py-20 px-5">
             <h2 className="code-pro-font uppercase text-center font-bold text-primaryBtn text-3xl md:text-5xl mb-10">Skills</h2>
-            <div className="flex justify-center items-center flex-wrap gap-6 ">
+            <Marquee autoFill={true} loop={0} pauseOnHover={true} className="flex items-center gap-6 ">
                 {
                     skills.map(skill => <div key={skill.id} className="hover:bg-secondaryBg rounded-xl p-3 shadow-lg shadow-primaryBtn">
                             <img src={skill.skillImage} alt={skill.skillName} className="w-10 md:w-14" />
                     </div>)
                 }
-            </div>
+            </Marquee>
         </div>
     );
 };
